7a71dc995b4f63518d52ac8343ba3c74b8ed8b5a
btreddy <btr.reddy@gmail.com>
2026-01-18 21:39:18 +0530

    Corrected Supabase Config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // REPLACE THESE WITH YOUR ACTUAL SUPABASE KEYS
-const supabaseUrl = 'sb_publishable_wvZQReYTE2f2afq0J9-a1g_0gl-pI5t';
-const supabaseKey = 'your-anon-public-key';
+const supabaseUrl = 'https://supabase.com/dashboard/project/ifqccwofrzajlifximqu';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmcWNjd29mcnphamxpZnhpbXF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NDczNjcsImV4cCI6MjA4NDMyMzM2N30.ldYpq67z9E-6J8hA8_6CQ59PFzS9nYsLfCAsksg-Xj8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
